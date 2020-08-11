ef0df23cd16f7086f42c597115510e9607d34c0c
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-11 12:00:52 +0300

    feat: controllers

--- BlogList/index.js
+++ BlogList/index.js
@@ -4,26 +4,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blogs");
+const blogsRouter = require("./controllers/blogs");
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
+app.use("/api/blogs", blogsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {

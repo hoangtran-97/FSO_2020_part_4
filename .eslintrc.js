8b60697ca71fae17dc9df3bf9cbadac56eaee22a
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-11 10:56:29 +0300

    feat: add dependancies

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2020: true,
+        node: true,
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {
+        eqeqeq: "error",
+        indent: ["error", 4],
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": ["error", "never"],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "no-unused-vars": [1],
+        "arrow-spacing": ["error", {before: true, after: true}],
+        "no-console": 0,
+    },
+};

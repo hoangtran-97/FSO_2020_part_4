f69018f4b6dfaf56325738b1cd3b76a75fe7dbfb
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-13 10:32:59 +0300

    feat: 4.4

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1;
 };
-
+const totalLikes = (blogs) => {
+    const reducer = (accumulator, blog) => {
+        return accumulator + blog.likes;
+    };
+    return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
 module.exports = {
     dummy,
+    totalLikes,
 };

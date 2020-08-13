ef828f74ee85877dc0d07df08cc88e9f536ee4ea
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-08-13 11:04:50 +0300

    feat: 4.5

--- BlogList/tests/list_helper.test.js
+++ BlogList/tests/list_helper.test.js
@@ -77,3 +77,15 @@ describe("total likes", () => {
         expect(listHelper.totalLikes(bigList)).toBe(36);
     });
 });
+describe("find favourite", () => {
+    test("blog with most likes", () => {
+        expect(listHelper.favoriteBlog(bigList)).toEqual({
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0,
+        });
+    });
+});
--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -7,7 +7,11 @@ const totalLikes = (blogs) => {
     };
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current), 0);
+};
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
